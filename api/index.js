1a22a0c23b8584ae71c267f7ef76a4e32c07d267
NoemiSauretG <noemisauretguillaumet@gmail.com>
2026-08-13 20:05:11 +0100

    vercel

--- api/index.js
+++ api/index.js
@@ -1,35 +1,19 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
 const cors = require("cors");
-const path = require("path");
-const multer = require("multer");
-const jwt = require("jsonwebtoken");
-const nodemailer = require("nodemailer");
-const { put, del } = require("@vercel/blob");
 
 const app = express();
 
-app.get("/api/test", (req, res) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: "ESTOY EN EL ÚLTIMO DEPLOYMENT"
-    });
-});
-
 app.use(cors());
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const PUBLIC_DIR = path.join(__dirname, "..", "public");
-app.use(express.static(PUBLIC_DIR));
 
 const db = mysql.createPool({
     host: process.env.MYSQLHOST,
     port: Number(process.env.MYSQLPORT || 3306),
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    ssl: process.env.MYSQL_SSL === "true" ? {} : undefined
+    database: process.env.MYSQLDATABASE
 });
 
 const upload = multer({
