34121a8e8eed20be26eb1b2eb2da3e955c0b4377
NoemiSauretG <noemisauretguillaumet@gmail.com>
2026-08-13 20:14:14 +0100

    vercel

--- api/index.js
+++ api/index.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
 const cors = require("cors");
+const path = require("path");
+const multer = require("multer");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const { put, del } = require("@vercel/blob");
 
 const app = express();
 
+app.get("/api/test", (req, res) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: "ESTOY EN EL ÚLTIMO DEPLOYMENT"
+    });
+});
+
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
+
+const PUBLIC_DIR = path.join(__dirname, "..", "public");
+app.use(express.static(PUBLIC_DIR));
 
 const db = mysql.createPool({
     host: process.env.MYSQLHOST,
     port: Number(process.env.MYSQLPORT || 3306),
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
+    database: process.env.MYSQLDATABASE,
+    ssl: process.env.MYSQL_SSL === "true" ? {} : undefined,
+    // 👇 AGREGA ESTAS LÍNEAS PARA VERCEL Y RAILWAY 👇
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 10000
 });
 
 const upload = multer({
